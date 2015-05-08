c591ee67afd66b81f96c0d9e5c753ea6fcd88d07
PrabinDahal <prabin.dahal@wwarn.org>
2015-05-08 17:09:57 +0100

    tidying up

--- Confidence interval for porpotion accounting for cluster effect.R
+++ Confidence interval for porpotion accounting for cluster effect.R
@@ -1,10 +1,10 @@
-##################################################################################################################################################
+##########################################################################################################################
 ## Title          : Function to work out confidence interval for correlated proportions using method by Fleiss et al, 2003
-## Reference BooK : Fleiss JL, Levin B, Paik MC, editors. Statistical methods for rates and proportions. New Jersey: Wiley; 2003. pp. 441-444.
+## Reference BooK : Fleiss, Levin and Paik.Statistical methods for rates and proportions. 2003. pp. 441-444.
 ## Equations used : Equations 15.2, 15.3 and 15.4 on pages 441-444 of the book
-## Version Date   : 06/12/13
+## Version Date   : 08/05/15
 ## Example data   : pups surivival data from Table 15.1 (p443) from the book
-##################################################################################################################################################
+###########################################################################################################################
 require(binom) # For computing Wilson's confidence interval
 
 ## k      = Number of clusters
@@ -29,19 +29,19 @@ fleiss<-function(data,yi,ni){
   if(j$K>1){			
   # Use Fliess's if the aggregate data comes from >1 cluster else report standard interval using Wilson's method
       if(!is.null(j)){
-      data$rho.com<-(data$yi*(data$yi-1))-(2*j$p*(data$ni-1)*data$yi)+(data$ni*(data$ni-1)*j$p*j$p)	        # Numerator of Equation (15.4)
-      data$rho.com.dn<-data$ni*(data$ni-1)*j$p*(1-j$p)											                                # Denominator of Equation (15.4)	
-      rho.hat<-sum(data$rho.com)/sum(data$rho.com.dn)								                                        # Equation (15.4) evaluated
+      data$rho.com<-(data$yi*(data$yi-1))-(2*j$p*(data$ni-1)*data$yi)+(data$ni*(data$ni-1)*j$p*j$p) # Numerator of Equation (15.4)
+      data$rho.com.dn<-data$ni*(data$ni-1)*j$p*(1-j$p)						    # Denominator of Equation (15.4)	
+      rho.hat<-sum(data$rho.com)/sum(data$rho.com.dn)						    # Equation (15.4) evaluated
     } 
   
     l<-as.data.frame(cbind(j,rho.hat))
     if(!is.null(l)){
-      vif<-1+((l$s2/l$n.a)+(l$n.a-1))*l$rho.hat 		                                                        # Equation (15.2)
+      vif<-1+((l$s2/l$n.a)+(l$n.a-1))*l$rho.hat  # Equation (15.2)
     } 
     
     m<-as.data.frame(cbind(l,vif))
     if(!is.null(m)){
-      var.p<-(m$p*(1-m$p)*m$vif)/sum(data$ni)			                                                          # Equation (15.3)
+      var.p<-(m$p*(1-m$p)*m$vif)/sum(data$ni)	# Equation (15.3)
     } 
     
     q<-as.data.frame(cbind(m,var.p))
@@ -53,7 +53,7 @@ fleiss<-function(data,yi,ni){
     return(data.frame(result))
   }else{ 
     # Wilson method outputs are numeric, whereas the fleiss output are characters. 
-    result<-data.frame(p=as.character(binom.confint(data$yi, data$ni, conf.level = 0.95, methods ="wilson")$mean),         # binom package solution
+    result<-data.frame(p=as.character(binom.confint(data$yi, data$ni, conf.level = 0.95, methods ="wilson")$mean),         # binom solution
                        lower=as.character(binom.confint(data$yi, data$ni, conf.level = 0.95, methods = "wilson")$lower),
                        upper=as.character(binom.confint(data$yi, data$ni, conf.level = 0.95, methods = "wilson")$upper),
                        method=as.character(binom.confint(data$yi, data$ni, conf.level = 0.95, methods = "wilson")$method))
@@ -61,11 +61,9 @@ fleiss<-function(data,yi,ni){
   }
 } # End;Not run
 
-
 #####################################
 ## Apply the code to pups data
 #####################################
-
 ## Pups data from the book in Table 15.1 (p. 443)
 
 pups<-data.frame(litter=c(seq(1,16)),
@@ -76,4 +74,3 @@ attach(pups)
 
 ## Apply the fuction
 fleiss(pups,yi,ni)
-
